b58accef95693d9105f0328f2e643fa533e587c3
MrDumbrava <109512837+9xbt@users.noreply.github.com>
2024-03-29 00:32:39 +0100

    Add caching avoider and update update

--- src/Webwatcher/Sites/config_about.js
+++ src/Webwatcher/Sites/config_about.js
@@ -3,7 +3,7 @@
 
 const checkforupdates_button = document.querySelector("#checkforupdates");
 const update_info = document.querySelector("#update_info");
-const latest_version = "https://raw.githubusercontent.com/9xbt/Webwatcher/main/api/latest_version"
+const latest_version = "https://raw.githubusercontent.com/9xbt/Webwatcher/main/api/latest_version?a=" + Math.random(); /* avoid caching */
 
 update_check();
 
@@ -16,10 +16,11 @@ function update_check() {
         .then(a => a.text())
         .then(latest_ver => {
             if (latest_ver == webwatcher_ver) {
-                update_info.textContent = "You have the latest version (version " + latest_ver + ")";
+                // TOOD: maybe make this say how long ago you actually checked, instead of it being hard coded
+                update_info.textContent = "You have the latest version (last checked just now)";
             }
             else {
-                update_info.textContent = "There's a new update available (version " + latest_ver + ")";
+                update_info.textContent = "There's a new update available (last checked just now)";
             }
         });
 }
