697652b4a6dace15d9b0a1e89e48ad0182bf56a4
MrDumbrava <109512837+9xbt@users.noreply.github.com>
2024-03-28 12:41:20 +0100

    Looking great

---
+++ src/Webwatcher/Sites/config_basic.js
@@ -0,0 +1,5 @@
+/* Webwatcher 1.9 Configuration Script */
+/* Copyright (c) 2022-2024 Mobren */
+
+const homepage_type = document.querySelector("#homepage_type");
+const hmoepage_url = document.querySelector("#homepage_url");
