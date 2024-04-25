700c6cdd280d4b8add1a83e77921443db17cb96f
xrc2 <109512837+9xbt@users.noreply.github.com>
2024-04-25 21:56:06 +0200

    Stuff

--- src/Webwatcher/Sites/config_about.js
+++ src/Webwatcher/Sites/config_about.js
@@ -2,12 +2,14 @@
 /* Copyright (c) 2022-2024 Mobren */
 
 const checkforupdates_button = document.querySelector("#checkforupdates");
+const showchangelog_button = document.querySelector("#showchangelog");
 const update_info = document.querySelector("#update_info");
 const latest_version = "https://raw.githubusercontent.com/9xbt/Webwatcher/main/api/latest_version?a=" + Math.random(); /* avoid caching */
 
 update_check();
 
 checkforupdates_button.onclick = update_check;
+showchangelog_button.onclick = show_changelog;
 
 function update_check() {
     update_info.textContent = "Checking for updates...";
@@ -22,4 +24,10 @@ function update_check() {
                 update_info.textContent = "There's a new update available on GitHub (last checked just now)";
             }
         });
+    }
+
+function show_changelog() {
+    var changelog = window.open('https://www.example.com', '_blank', 'width=600,height=400');
+
+    changelog.focus();
 }
