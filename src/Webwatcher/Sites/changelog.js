c10284a0882002c5e74c2303952d149366c7e10d
xrc2 <109512837+9xbt@users.noreply.github.com>
2024-04-27 14:54:25 +0200

    Bug fixes

---
+++ src/Webwatcher/Sites/changelog.js
@@ -0,0 +1,13 @@
+﻿/* Webwatcher 1.9 changelog page script */
+/* Copyright (c) 2022-2024 Mobren */
+
+const navbar = document.querySelector(".navbar");
+const urlParams = new URLSearchParams(window.location.search);
+
+run_animation();
+
+function run_animation() {
+    if (!urlParams.has('disable_animations')) {
+        navbar.classList.add("navbar_with_animation");
+    }
+}
